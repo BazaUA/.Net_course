de161d2b6939314f534d4094539524932f47a321
BazaUA <vbazalickyi@gmail.com>
2018-01-28 12:12:10 +0200

    HW1

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("vitaliy", 91, 89, 100);
+            Student s2 = new Student("max", 100, 81, 87);
+            Student s3 = new Student("olha", 71, 100, 91);
+            Student s4 = new Student("gleb", 94, 100, 76);
+            Student s5 = new Student("dmytro", 90, 100, 66);
+            var students = new List<Student>();
+            students.Add(s1);
+            students.Add(s2);
+            students.Add(s3);
+            students.Add(s4);
+            students.Add(s5);
+            students.Sort((student1, student2) => student2.getAvg().CompareTo(student1.getAvg()));
+            foreach(Student s in students)
+            {
+                Console.WriteLine(s.ToString()+"\n--------------");
+            }
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ HW1/HW1/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW1
+{
+    class Student
+    {
+        public string Name { get; private set; }
+        public int MathScore { get; private set; }
+        public int JavaScore { get; private set; }
+        public int DotNetScore { get; private set; }
+
+        public Student(string Name, int MathScore, int JavaScore, int DotNetScore)
+        {
+            this.Name = Name;
+            this.MathScore = MathScore;
+            this.JavaScore = JavaScore;
+            this.DotNetScore = DotNetScore;
+        }
+
+        public double getAvg()
+        {
+            return ((MathScore+JavaScore+DotNetScore)/3.0);
+        }
+
+        public override string ToString()
+        {
+            return "Name: "+Name+"\n math score :"+MathScore+ "\n java score :" + JavaScore+ "\n .NET score :" + DotNetScore+ "\n average score: "+getAvg();
+        }
+
+
+    }
+}
